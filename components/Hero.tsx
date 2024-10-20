f0ee82b7fdd746d62fdffb080e457188c913b4f1
Ammar Khan <ammar.khaan05@gmail.com>
2024-10-21 01:52:01 +0500

    updated component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section>
+        <div className="border-2 border-red-500 max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+            {/*LEFT*/}
+            <h1 className="bold-52 lg:bold-88">
+                Welcome to the Computer Sceince Society
+            </h1>
+
+            {/*RIGHT*/}
+            <div className="border-2 border-blue-500 flex relative flex-1 items-start">
+                
+            </div>
+        </div>
+    </section>
   )
 }
 
