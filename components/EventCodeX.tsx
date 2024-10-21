9ba6f18df1a81102688fb5ed3326ddef609f6586
Ammar Khan <ammar.khaan05@gmail.com>
2024-10-21 11:42:31 +0500

    added more components

---
+++ components/EventCodeX.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventCodeX = () => {
+  return (
+    <div>EventCodeX</div>
+  )
+}
+
+export default EventCodeX
---
+++ components/EventProBattle.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventProBattle = () => {
+  return (
+    <div>EventProBattle</div>
+  )
+}
+
+export default EventProBattle
