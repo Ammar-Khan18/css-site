6cf50a62efd4974cd6e528d44f02d13b18287d89
Ammar Khan <ammar.khaan05@gmail.com>
2024-10-21 11:42:17 +0500

    modified page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import AboutUs from "@/components/AboutUs";
+import EventCodeX from "@/components/EventCodeX";
+import EventProBattle from "@/components/EventProBattle";
 import Hero from "@/components/Hero";
+import Socials from "@/components/Socials";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <EventCodeX />
+      <EventProBattle />
+      <Socials />
+      <AboutUs />
     </>
   );
 }
