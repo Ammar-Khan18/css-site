c89738a2d047bba5fc65fc6757805747b54b6156
Ammar Khan <ammar.khaan05@gmail.com>
2024-10-21 11:32:36 +0500

    updated components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
   return (
     <section>
-        <div className="border-2 border-red-500 max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
-            {/*LEFT*/}
-            <h1 className="bold-52 lg:bold-88">
-                Welcome to the Computer Sceince Society
-            </h1>
+        <div className="border-2 border-red-500 max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row align-middle justify-center">
+          {/*LEFT*/}
+            <div className="border-2 border-green-500 relative z-20 flex flex-1 flex-col xl:w-1/2">
+              <p className="text-gray-20">Introducing the</p>
+              <h1 className="bold-52 lg:bold-88">Computer Science Society</h1>
+              <p className="text-white">Are you passionate about the world of computers and technology? Join the IBA Computer Science Society, where we're dedicated to fostering growth and innovation in the field. Through exciting competitions, hackathons, workshops, and guest speaker sessions, we provide a platform for students to explore the latest trends, develop their skills, and connect with like-minded individuals.</p>
+            </div>
 
-            {/*RIGHT*/}
-            <div className="border-2 border-blue-500 flex relative flex-1 items-start">
-                
+          {/*RIGHT*/}
+            <div className="border-2 border-blue-500 flex relative w-[30%] items-start align-middle justify-center xs:self-center">
+              <Image src="/css_transparent.png" alt="Logo" width={500} height={500} className="h-auto"/>
             </div>
         </div>
     </section>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,14 +8,13 @@ const Navbar = () => {
       <Link href={"/"}>
         <Image src="/CSS_white_logo.png" alt="Logo" width={74} height={29}/>
       </Link>
-      <ul className="hidden h-full gap-12 lg:flex">
+      <ul className="xs:flex h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
           <Link href={link.href} key={link.key} className="regular-16 text-white flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
             {link.label}
           </Link>
         ))}
       </ul>
-      <Image src="menu.svg" alt="menu" width={32} height={32} className="inline-block cursor-pointer lg:hidden"/>
     </nav>
   )
 }
